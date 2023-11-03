9a0eec461de1d8696f2e69164ecb5a1b364f31bf
Mohamed Ibrahim <mohamedibrahimabdulghani@gmail.com>
2023-11-03 11:50:07 +0200

    delete page

--- handlers/delete-task.php
+++ handlers/delete-task.php
@@ -4,9 +4,15 @@ require_once "../functions/app.php";
 
 if(isset($_GET["id"])):
     $ID = $_GET["id"];
-    $result = deleteTasks($ID);
+    $result = selectTaskById($ID);
+    if($result):
+        $res = deleteTasks($ID);
         $_SESSION["success"] = "Data deleted successfully";
-        header("Location: ../index.php");
+    else:
+        $_SESSION["error"] = "Data not exist";
+    endif;
+    header("Location: ../index.php");
+
 else:
     header("Location: ../index.php");
 endif;
